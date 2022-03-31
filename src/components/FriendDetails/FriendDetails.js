0d2365aa2152e20ddc27cc5c0989da123ca5e36d
Soyeb <tssoyeb@gmail.com>
2022-03-31 19:44:19 +0600

    data show in ui with dynamic data

--- src/components/FriendDetails/FriendDetails.js
+++ src/components/FriendDetails/FriendDetails.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const FriendDetails = () => {
-    let params = useParams();
+    let {friendId} = useParams();
+    const [friend,setfriend] =useState({})
+    useEffect( ()=>{
+        const url =`https://jsonplaceholder.typicode.com/users/${friendId}`
+        console.log(url);
+        fetch(url)
+        .then(res =>res.json())
+        .then(data =>setfriend(data) )
+    },{})
     return (
         <div>
-            <h1>Specific friend. {params.friendId} </h1>
+            <h1>Specific friend. {friendId} </h1>
+            <h1>Name : {friend.name}</h1>
+            <h1>username : {friend.username}</h1>
+            <h1>City :{ friend?.address?.city} </h1>
         </div>
     );
 };
