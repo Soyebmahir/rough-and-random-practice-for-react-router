d891b5049284a0c4f7ec514c1ab1773596d39d98
Soyeb <tssoyeb@gmail.com>
2022-03-31 09:07:28 +0600

    first commit

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>This About part</h1>
+            <h3>You can find out about this site</h3>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>This is home</h1>
+            <h3>Here You can find every topic's short form and Direction where to go</h3>
+        </div>
+    );
+};
+
+export default Home;
